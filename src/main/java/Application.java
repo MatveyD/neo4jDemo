b747f69995dde4ffad6d35b82329b78421ad67a2
MatveyD <matv1995@gmail.com>
2017-03-23 02:33:01 +0300

    Neo4j Demo version 0.9

---
+++ src/main/java/Application.java
@@ -0,0 +1,47 @@
+/**
+ * Created by matv1 on 22.03.2017.
+ */
+// NOTE: Be careful about auto-formatting here: All imports should be between the tags below.
+// tag::minimal-example-import[]
+
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.neo4j.driver.v1.*;
+
+import java.io.IOException;
+
+import static org.neo4j.driver.v1.Values.parameters;
+// end::minimal-example-import[]
+
+public class Application {
+    public static void main(String[] args) throws InvalidFormatException,
+            IOException {
+        {
+            String label ="Place";
+            String query ="MATCH (a:"+label+") WHERE a.from = {from} RETURN a.name AS name, a.from AS title";
+            // tag::minimal-example[]
+            Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "69WhWg5"));
+
+            try (Session session = driver.session()) {
+
+                try (Transaction tx = session.beginTransaction()) {
+                    tx.run("CREATE (a:Person {name: {name}, title: {title}})",
+                            parameters("name", "Arthur", "title", "King"));
+                    tx.success();
+                }
+
+                try (Transaction tx = session.beginTransaction()) {
+                    StatementResult result = tx.run(query,
+                            parameters("from", "Sweden"));
+                    while (result.hasNext()) {
+                        Record record = result.next();
+                        System.out.println(String.format("%s %s", record.get("name").asString(), record.get("title").asString()));
+                    }
+                }
+
+            }
+
+            driver.close();
+            // end::minimal-example[]
+        }
+    }
+}
