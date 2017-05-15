9441dfeda950046ff80e8dd3401fe11c2e3e8039
MatveyD <matv1995@gmail.com>
2017-05-15 15:50:52 +0300

    pilot

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -16,8 +16,24 @@ public class Application {
     public static void main(String[] args) throws InvalidFormatException,
             IOException {
         {
-            String label ="Place";
-            String query ="MATCH (a:"+label+") WHERE a.from = {from} RETURN a.name AS name, a.from AS title";
+            String questionType = "Who";
+            String label1 = "";
+            String label2 = "";
+            String relation = "ACTED_IN";
+            String relationPropertyType1 = "roles:";
+            String relationToken1 = "'Jimmy Dugan'";
+            String tokenType1 ="name";
+            String token1 ="Tom Hanks";
+            if (questionType.equals("Who"))
+                label1 =":Person";
+            if (!relation.equals("")) {
+                relationPropertyType1 = "{" + relationPropertyType1;
+                relationToken1 = relationToken1 + "}";
+            }
+            //String query ="MATCH (a:"+label+") WHERE a."+relation+" = {from} RETURN a.name AS name, a.from AS title";
+            //"MATCH (a:"+label+") WHERE a.from = {from} RETURN a.name AS name, a.from AS title"
+            //String query ="MATCH (n1:"+label+")-[r:"+relation+"]->(n2) where n2.title = {title} RETURN n1, r, n2";
+            String query = "MATCH (n1"+label1+" {"+tokenType1+": {token}})-[r:"+relation+ relationPropertyType1 + relationToken1 +"]->(n2"+ label2 +") RETURN n1.name ,n2.title";
             // tag::minimal-example[]
             Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", ""));
 
@@ -31,10 +47,10 @@ public class Application {
 
                 try (Transaction tx = session.beginTransaction()) {
                     StatementResult result = tx.run(query,
-                            parameters("from", "Sweden"));
+                            parameters("token", token1));
                     while (result.hasNext()) {
                         Record record = result.next();
-                        System.out.println(String.format("%s %s", record.get("name").asString(), record.get("title").asString()));
+                        System.out.println(String.format("%s %s", record.get("n1.name").asString(), record.get("n2.title").asString()));
                     }
                 }
 
